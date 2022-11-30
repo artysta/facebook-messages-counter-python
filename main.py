454356d3cce8890aa1179f7e28596fa0a40e0e88
Adrian Kurek <adrian.kurek93@gmail.com>
2022-11-30 10:13:50 +0100

    Move the code into a function

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import os
 def get_percent(number, total_number):
     return round(number / total_number * 100, 2)
 
-names_to_messages = {}
-total_number_of_messages = 0
+def count_all_messages():
+    names_to_messages = {}
+    total_number_of_messages = 0
 
-for filename in os.listdir('./'):
-    if '.json' not in filename:
-        continue
-    
-    file = open(filename, 'r')
-    data_json = json.load(file)
-    messages = data_json['messages']
+    for filename in os.listdir('./'):
+        if '.json' not in filename:
+            continue
+        
+        file = open(filename, 'r')
+        data_json = json.load(file)
+        messages = data_json['messages']
 
-    for message in messages:
-        sender_name = message['sender_name']
-        total_number_of_messages += 1
+        for message in messages:
+            sender_name = message['sender_name']
+            total_number_of_messages += 1
 
-        if sender_name in names_to_messages:
-            names_to_messages[sender_name] = names_to_messages[sender_name] + 1
-        else:
-            names_to_messages[sender_name] = 0
+            if sender_name in names_to_messages:
+                names_to_messages[sender_name] = names_to_messages[sender_name] + 1
+            else:
+                names_to_messages[sender_name] = 0
+        
+    print(f'Całkowita liczba wiadomości {total_number_of_messages}.')
 
-print(f'Całkowita liczba wiadomości {total_number_of_messages}.')
+    for k, v in names_to_messages.items():
+        print(f'{k} napisał {v} ({get_percent(v, total_number_of_messages)}%) wiadomości.')
 
-for k, v in names_to_messages.items():
-    print(f'{k} napisał {v} ({get_percent(v, total_number_of_messages)}%) wiadomości.')
+count_all_messages()
